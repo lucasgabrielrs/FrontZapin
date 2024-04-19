9e8909ee50bb22912a75290e304f4b017e2920d8
Lucas Gabriel <lucasgabrielrs123@gmail.com>
2024-04-19 10:52:26 -0300

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import { RiSendPlaneFill } from "react-icons/ri";
 
 export function Footer() {
   return (
-    <div className="pt-2 pb-[env(safe-area-inset-bottom, 20px)] bg-white">
+    <div className="pt-2 pb-[env(safe-area-inset-bottom, 20px)]">
       <div className="md:absolute fixed left-3 bottom-3 rounded-lg border-solid border-2 border-primaryOrange w-3/4 bg-laranjinha" style={{ marginBottom: 'env(safe-area-left-bottom)' }}>
         <input
           className="appearance-none border-none m-0 bg-transparent focus:outline-none w-full p-2"
